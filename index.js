330897d53ef3987b754e0ab97a3f26b979b25583
Purnendu Shukla <pshukla_be15@thapar.edu>
2019-06-11 12:35:52 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message:' + msg);
+        io.emit('chat message', msg);
+    })
+})
+
+http.listen(port,()=>{
+    console.log("Server started on port:"+port);
+})
