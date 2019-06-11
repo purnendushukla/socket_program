6cbe16a8a5f60aa81ac2e0655486c15159a6f0b4
Purnendu Shukla <pshukla_be15@thapar.edu>
2019-06-11 14:50:34 +0530

    open chatting system working

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
+// const express = require('express');
+// const app = express();
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
+// const port = 3000;
+//
+// app.get('/', (req, res)=>{
+//     // res.sendFile(__dirname+'/index.html');
+//     res.render('index.ejs')
+// });
+//
+// io.on('connection', function(socket){
+//     socket.on('chat message', function(msg){
+//         console.log('message:' + msg);
+//         io.emit('chat message', msg);
+//     })
+// })
+//
+// http.listen(port,()=>{
+//     console.log("Server started on port:"+port);
+// })
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 3000;
 
-app.get('/', (req, res)=>{
-    // res.sendFile(__dirname+'/index.html');
-    res.render('index.ejs')
+app.get('/', function(req, res) {
+    res.render('index.ejs');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        console.log('message:' + msg);
-        io.emit('chat message', msg);
-    })
-})
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+    });
 
-http.listen(port,()=>{
-    console.log("Server started on port:"+port);
-})
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+});
+
+const server = http.listen(8080, function() {
+    console.log('listening on *:8080');
+});
